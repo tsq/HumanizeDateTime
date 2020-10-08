d51960b22e27079548bb626f4eed7b0341577599
tsq <tangshiqiangcn@gmail.com>
2020-10-09 04:05:41 +0800

    Add HumanizeDateTime

---
+++ HumanizeDateTime.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Tsq.HumanizeDateTime
+{
+    public static class HumanizeDateTime
+    {
+        public static string Humanize(this DateTime dateTime)
+        {
+            const int SECOND = 1;
+            const int MINUTE = 60 * SECOND;
+            const int HOUR = 60 * MINUTE;
+            const int DAY = 24 * HOUR;
+            const int MONTH = 30 * DAY;
+
+            var ts = new TimeSpan(DateTime.UtcNow.Ticks - dateTime.Ticks);
+            double delta = Math.Abs(ts.TotalSeconds);
+            if (delta < 1 * MINUTE)
+                return ts.Seconds == 1 ? "1秒前" : ts.Seconds + "秒前";
+
+            if (delta < 2 * MINUTE)
+                return "1分钟前";
+
+            if (delta < 45 * MINUTE)
+                return ts.Minutes + "分钟前";
+
+            if (delta < 90 * MINUTE)
+                return "1小时前";
+
+            if (delta < 24 * HOUR)
+                return ts.Hours + "小时前";
+
+            if (delta < 48 * HOUR)
+                return "昨天";
+
+            if (delta < 30 * DAY)
+                return ts.Days + "天前";
+
+            if (delta < 12 * MONTH)
+            {
+                int months = Convert.ToInt32(Math.Floor((double)ts.Days / 30));
+                return months <= 1 ? "1个月前" : months + "个月前";
+            }
+            else
+            {
+                int years = Convert.ToInt32(Math.Floor((double)ts.Days / 365));
+                return years <= 1 ? "1年前" : years + "年前";
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,55 @@
 # HumanizeDateTime
-将一个Datetime类型变量转化成易读的字符串
+
+将一个Datetime类型的变量转化成易读的中文字符串，例如：10秒前、2分钟前、4小时前、3天前等。
+
+### 用途
+
+常用于前端页面显示易读的日期，如论坛中，帖子的发表日期，评论的回复日期等。
+
+### 安装
+
+```sh
+dotnet add package Tsq.HumanizeDateTime
+```
+
+也可参考： https://www.nuget.org/packages/Tsq.HumanizeDateTime
+
+### 使用效果
+
+![](./screenshot.png)
+
+### 使用方法
+
+第一步：引入包
+
+```cs
+using Tsq.HumanizeDateTime
+```
+
+第二步：Datetime类型的变量直接调用`Humanize()`方法
+
+```cs
+dateTime.Humanize(); 
+```
+
+一个完整的Demo
+
+```cs
+using System;
+using Tsq.HumanizeDateTime;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            var date = Convert.ToDateTime("06/07/2010").Humanize();
+            Console.WriteLine(date);
+        }
+    }
+}
+
+```
+
